81870ab9930562efac022cab695e00e9646ed149
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-03-26 12:44:51 -0300

    feat: criado as relações para vincular as pessoas as devidas facções

--- criminal_management/urls.py
+++ criminal_management/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.FactionCreateView.as_view(), name='faction-create') 
+    path('', views.FactionCreateView.as_view(), name='faction-create'),
+    path('select/', views.FactionSelectView.as_view(), name='select-faction'),
+    path('add_member/', views.FactionUpdateView.as_view(), name='add-member'),
 ]
--- criminal_management/views.py
+++ criminal_management/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.views import generic
 from .models import Faction
 from .forms import FactionForm
+from persons.models import Person
 # Create your views here.
 
 class FactionCreateView(generic.CreateView):
@@ -8,3 +10,28 @@ class FactionCreateView(generic.CreateView):
     form_class = FactionForm
     template_name = 'create_faction.html'
     success_url = '/persons/'
+
+
+class FactionSelectView(generic.TemplateView):
+    template_name = 'select_factions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        person_id = self.request.GET.get("id_person")
+        factions = Faction.objects.all()
+        person = Person.objects.get(pk=person_id)
+        context["person"] = person
+        context["factions"] = factions
+
+        return context
+
+
+class FactionUpdateView(generic.View):
+    
+    def post(self,request, *args, **kwargs):
+        faction_id = request.POST.get('opcoes')
+        person_id = request.POST.get('person')
+        faction = Faction.objects.get(pk=faction_id)
+        person = Person.objects.get(pk=person_id)
+        faction.member.add(person)
+        return HttpResponse("Vamos ver")
